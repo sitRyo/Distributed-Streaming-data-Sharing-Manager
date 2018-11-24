db2bac86a7dc10937290c842b130ad4e95af6572
sitRyo <al15030@shibaura-it.ac.jp>
2018-11-24 16:08:52 +0900

    シェルスクリプト追加

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+RM=/bin/rm
+MKDIR=/bin/mkdir
+MAKE=/usr/bin/make
+DIR=build2
+LIBNAME=libssm.so.0
+
+pt=`pwd`
+
+${MKDIR} ../build2
+./configure --preifx=${pt}/../${DIR}/lib64/${LIBNAME}.org
+${MAKE}
+${MAKE} install
+
+mv ../${DIR}/lib64/${LIBNAME} ../${DIR}/lib64/${LIBNAME}.org
+cd ../build2/lib64
+ln -s ${pt}/src/.libs/libssm.so.0 .
