bf4fbca2352eb9f9ec9f3eeb0034694cc60bb6bf
sitRyo <al15030@shibaura-it.ac.jp>
2020-05-07 18:51:56 +0900

    update_ssmを修正

--- update_ssm.sh
+++ update_ssm.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
+
+## Makefileの更新からmake installまでの処理を行うスクリプト
+
 SSMSOURCE="/home/seriru/Researchment/Distributed-Streaming-data-Sharing-Manager"
 
+output_message() {
+  printf "\n**************************\n"
+  printf "\033[1m%s\033[m\n" "$(date '+%y/%m/%d %H:%M:%S')  "
+  printf "\033[1;$1m%s\033[m\n" "$2"
+  printf "**************************\n"
+}
+
 cd ${SSMSOURCE}
-autoheader
-aclocal
-automake -a -c
-./configure 
+
+option=$1
+if [[ $option = "-fixed" ]]; then
+  autoheader
+  aclocal
+  automake -a -c
+  sudo make distclean
+  ./configure 
+fi
+
+if [[ $option = "-help" ]]; then
+  echo "help" 
+  echo "-fixed: Autotools, configure実行"
+  echo "-help: helpメッセージを表示"
+  exit 0
+fi
+
 sudo make CFLAGS="-Wno-error" CXXFLAGS="-Wno-error"
+
+## makeの正常終了コードは0なので、それ以外のときはエラー処理
+answer=$?
+if [ $answer -ne 0 ]; then
+  output_message 31 "build failed"
+  exit 0 # 途中終了
+fi
+
 sudo make install
-printf "\n**************************\n"
-printf "\033[1m%s\033[m\n" "$(date '+%y/%m/%d%H:%M:%S')  "
-printf "\033[1;36m%s\033[m\n" "build finish"
-printf "**************************\n"
+
+output_message 36 "build finish"
