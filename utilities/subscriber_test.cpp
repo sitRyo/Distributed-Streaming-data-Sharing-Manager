b51b00867a8c0d3f3b228a68f22fdf8defd5913a
sitRyo <al15030@shibaura-it.ac.jp>
2020-05-27 14:31:17 +0900

    subscriber_testを修正

--- utilities/subscriber_test.cpp
+++ utilities/subscriber_test.cpp
@@ -21,10 +21,13 @@ int main() {
   // Subscriber情報
   std::vector<SubscriberSet> subscriber_set;
 
+  Stream int_ssm1 {SNAME_INT, 0, sizeof(int), 0};
+  Stream int_ssm2 {SNAME_INT, 1, sizeof(int), 0};
+
   // SSMApiの情報をここに入力する。
   // StreamName, StreamId, dataのサイズ, propertyのサイズ
-  streams.push_back({SNAME_INT, 0, sizeof(int), 0});
-  streams.push_back({SNAME_INT, 1, sizeof(int), 0});
+  streams.push_back(int_ssm1);
+  streams.push_back(int_ssm2);
 
   // ストリームを追加。
   sub.add_stream(streams);
@@ -34,8 +37,8 @@ int main() {
   
   // Subscribeするストリーム情報を設定する。
   // StreamName, StreamId, dataのサイズ, propertyのサイズ, 条件の指定
-  SubscriberSet ss({SNAME_INT, 0, sizeof(int), 0}, OBSV_COND_LATEST);
-  SubscriberSet ss2({SNAME_INT, 1, sizeof(int), 0}, OBSV_COND_LATEST);
+  SubscriberSet ss(int_ssm1, OBSV_COND_LATEST);
+  SubscriberSet ss2(int_ssm2, OBSV_COND_LATEST);
   subscriber_set.emplace_back(ss);
   subscriber_set.emplace_back(ss2);
 
@@ -43,23 +46,18 @@ int main() {
   bool flag = true;
   
   // ローカル条件
-  auto cond = [&flag]() -> bool {
+  std::function<bool()> local_cond = [&flag]() -> bool {
     return flag;
   };
 
   // コールバック
-  auto callback = [](intSsm_k data1, intSsm_k data2) {
+  std::function<void(intSsm_k, intSsm_k)> callback = [](intSsm_k data1, intSsm_k data2) {
     printf("data: %d\n", data1.num + data2.num);
   };
 
-  // ローカル条件は bool()
-  std::function<bool()> local_cond = cond;
-  // コールバックはvoid(Args...)
-  std::function<void(intSsm_k, intSsm_k)> print = callback;
-
   // subscriberを登録。
   // 使用したいsubscriberごとにinvokeする。
-  sub.register_subscriber(subscriber_set, local_cond, print);
+  sub.register_subscriber(subscriber_set, local_cond, callback);
 
   // subscriberを開始する。
   sub.start();
