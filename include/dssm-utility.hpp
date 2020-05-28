48f94941772ba3a0a5e6443967693e018eaacaf6
sitRyo <al15030@shibaura-it.ac.jp>
2020-05-28 17:30:18 +0900

    ssmmsgのサイズ計算を修正

--- include/dssm-utility.hpp
+++ include/dssm-utility.hpp
@@ -20,7 +20,7 @@ namespace dssm {
 		 * 16byteずつhexdumpする。
 		 */
 		void hexdump(char *p, uint32_t len) {
-			for (int i = 0; i < len; ++i) {
+			for (auto i = 0; i < len; ++i) {
 				if (i % 16 == 0 and i != 0) std::printf("\n");
 				std::printf("%02X ", p[i] & 0xff);
 			}
@@ -53,7 +53,7 @@ namespace dssm {
 			len += sizeof(msg.res_type);
 			len += sizeof(msg.cmd_type);
 			len += sizeof(msg.name);
-			len += sizeof(msg.ssize);
+			len += sizeof(msg.suid);
 			len += sizeof(msg.ssize);
 			len += sizeof(msg.hsize);
 			len += sizeof(msg.time);
@@ -62,34 +62,7 @@ namespace dssm {
 			return len;
 		}
 
-		/**
-		 * @brief タプルを関数の引数に展開し, std::__invokeを呼び出す。
-		 */
-		template <class Fn, class Tuple, size_t... _Idx>
-		constexpr decltype(auto)
-		apply_impl(Fn&& f, Tuple&& t, std::index_sequence<_Idx...>) {
-			return std::__invoke(
-				std::forward<Fn>(f),
-				std::get<_Idx>(std::forward<Tuple>(t))...
-			);
-		}
 
-		/**
-		 * @brief apply(f, t)でfにtupleを展開し実行する。apply_implへのアダプタ。c++14で動作可能。
-		 */
-		template <class Fn, class Tuple>
-		constexpr decltype(auto)  // この時点で戻り値はわからない
-		apply(Fn&& f, Tuple&& t) {
-			using Indices 
-				= std::make_index_sequence<
-					std::tuple_size<std::remove_reference_t<Tuple>>::value
-				>;
-			return apply_impl(
-				std::forward<Fn>(f),
-				std::forward<Tuple>(t),
-				Indices{}
-			);
-		}
 
 	} // namespace util
 } // namespace dssm
